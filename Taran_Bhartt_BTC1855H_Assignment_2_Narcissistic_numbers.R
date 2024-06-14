e58c390f392d136a22b7491449370c9df65b6234
Taran Bhartt <Tarzor101@gmail.com>
2024-06-14 17:51:36 -0400

    Wrote code for the user to input a prompt Wrote code to analyze the input, whether or not it is a three digit positive number

--- Taran_Bhartt_BTC1855H_Assignment_2_Narcissistic_numbers.R
+++ Taran_Bhartt_BTC1855H_Assignment_2_Narcissistic_numbers.R
@@ -1,6 +1,7 @@
 #' 1. Prompt the user to enter a three digit positive number
 #' Plan: 
 #'       Prompt the user to input a 3 digit positive number using the readline function
+UserNumber <- readline(prompt = "Input a three digit number: ")
 
 #' 2. Check if the user input is numeric. If not, print an error message and quit.
 #' Plan: 
@@ -9,6 +10,15 @@
 #'          Convert the user input into a numeric value
 #'       else
 #'          Write an error message
+
+if ((as.numeric(UserNumber))>0 & (nchar(UserNumber))==3){
+  UserNumber <- as.numeric(UserNumber)
+  print(paste("You entered", UserNumber)) # determine if the input value is a 3 digit positive number
+} else if (is.na(as.numeric(UserNumber))){
+  print("Please enter a number instead of an NA") # determine if the input is an NA
+} else {
+  print("You did not input a three digit positive number") # if it is a non-three digit number, tell them to reinput the number
+} # 
 
 #' 3. Check if the number is narcissistic. A narcissistic number, or an Armstrong
 #'    number, is a number that is equal to the sum of the cubes of its own digits. 
@@ -22,4 +32,5 @@
 #'    or any additional fun text you may want to add.
 #' plan: 
 #'      Use the print and paste function to display the message
+#'      Include a function to restart the program or press a key to escape
 
