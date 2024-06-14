a89f42271d55bbe70f5d5c1ced7a6f200def1fd3
Taran Bhartt <Tarzor101@gmail.com>
2024-06-14 17:36:11 -0400

    Updated plan that includes more specific functions

--- Taran_Bhartt_BTC1855H_Assignment_2_Narcissistic_numbers.R
+++ Taran_Bhartt_BTC1855H_Assignment_2_Narcissistic_numbers.R
@@ -1,24 +1,25 @@
 #' 1. Prompt the user to enter a three digit positive number
-#' Plan: Need to include a print function for the query
-#'       Need to figure out how to ask for a prompt
-#'       Might need to be a readline function
+#' Plan: 
+#'       Prompt the user to input a 3 digit positive number using the readline function
 
 #' 2. Check if the user input is numeric. If not, print an error message and quit.
-#' Plan: Will probably need to use the is.numeric function
-#'       Will also need to use a print function, where the output will vary
-#'       depending on the logical result of the is.numeric function
-
+#' Plan: 
+#'       Check if the user actually input a 3 digit positive number
+#'       if T
+#'          Convert the user input into a numeric value
+#'       else
+#'          Write an error message
 
 #' 3. Check if the number is narcissistic. A narcissistic number, or an Armstrong
 #'    number, is a number that is equal to the sum of the cubes of its own digits. 
 #'    153, 370, 371, 407 are three digit Armstrong numbers.
-#' plan: Similar setup to the above number, though there is an initial step
-#'       Would it be easier to break it up as a string?
-#'       There may be a separate function
-
+#' plan: 
+#'      Break up the number
+#'      Check if the number is an Armstrong number
 
 #' 4. Display the result with an appropriate message, e.g. 
 #'    “127 is not an Armstrong number” or “370 is a narcissistic number” 
 #'    or any additional fun text you may want to add.
-#' plan: similar steps to checking whether or not the input is numeric
+#' plan: 
+#'      Use the print and paste function to display the message
 
