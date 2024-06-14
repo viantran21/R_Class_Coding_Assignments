57c3af2f4751091cf41aa4f705b7ed42a4f3f44c
Taran Bhartt <Tarzor101@gmail.com>
2024-06-14 16:26:17 -0400

    Initial commit of Assignment 2: Narcissistic Numbers Includes a plan for how to approach the project

---
+++ Taran_Bhartt_BTC1855H_Assignment_2_Narcissistic_numbers.R
@@ -0,0 +1,24 @@
+#' 1. Prompt the user to enter a three digit positive number
+#' Plan: Need to include a print function for the query
+#'       Need to figure out how to ask for a prompt
+#'       Might need to be a readline function
+
+#' 2. Check if the user input is numeric. If not, print an error message and quit.
+#' Plan: Will probably need to use the is.numeric function
+#'       Will also need to use a print function, where the output will vary
+#'       depending on the logical result of the is.numeric function
+
+
+#' 3. Check if the number is narcissistic. A narcissistic number, or an Armstrong
+#'    number, is a number that is equal to the sum of the cubes of its own digits. 
+#'    153, 370, 371, 407 are three digit Armstrong numbers.
+#' plan: Similar setup to the above number, though there is an initial step
+#'       Would it be easier to break it up as a string?
+#'       There may be a separate function
+
+
+#' 4. Display the result with an appropriate message, e.g. 
+#'    “127 is not an Armstrong number” or “370 is a narcissistic number” 
+#'    or any additional fun text you may want to add.
+#' plan: similar steps to checking whether or not the input is numeric
+
