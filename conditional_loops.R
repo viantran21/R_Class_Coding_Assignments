4f6db398de662b22c0cfeb28fff43de87cdb2015
Taran Bhartt <Tarzor101@gmail.com>
2024-06-21 16:58:36 -0400

    Infinite while loop

---
+++ conditional_loops.R
@@ -0,0 +1,10 @@
+# while and repeat loop examples
+
+# while loop INFINITE
+
+i <- 1 #initialize the counter
+
+while (i < 3) {
+  print(i)
+}
+# this will run forever. If you were to run this then a little stop button would pop up in the console, you could also press ctrl-c
